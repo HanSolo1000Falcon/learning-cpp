082f1969731be1f590c1f8286e7e26c76942fb66
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2026-03-10 22:46:40 +0100

    Simple non working tictactoe implementation with some code in main so that's bad (gonna have to do like I did in RPS)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,42 @@
-#include "rps/rps_manager.h"
+#include "ttt/tictactoe_board.hpp"
+
+tictactoe_coordinate parse_coordinate(const std::string &coordinate_string) {
+    const auto comma = coordinate_string.find(',');
+    if (comma == std::string::npos) {
+        throw std::invalid_argument("missing comma");
+    }
+    return {std::stoi(coordinate_string.substr(0, comma)), std::stoi(coordinate_string.substr(comma + 1))};
+}
 
 int main() {
-    rps_manager::init_game_and_start_game_loop();
+    auto ttt_board = tictactoe_board();
+    auto was_x = false;
+    while (true) {
+        was_x = !was_x;
+        ttt_board.draw_board();
+        std::cout << "\nPlease enter your move (x,y): ";
+
+        tictactoe_coordinate coordinate{};
+        bool valid = false;
+        while (!valid) {
+            std::string move;
+            std::cin >> move;
+            try {
+                coordinate = parse_coordinate(move);
+
+                if (coordinate.x > tictactoe_board::board_size - 1 || coordinate.y > tictactoe_board::board_size - 1 ||
+                    ttt_board.is_occupied(coordinate) || coordinate.x < 0 || coordinate.y < 0) {
+                    std::cout << "Invalid coordinate, try again (x,y): ";
+                    continue;
+                }
+
+                valid = true;
+            } catch (const std::invalid_argument &e) {
+                std::cout << "Bad input, try again (x,y): ";
+            }
+        }
+
+        ttt_board.play_move(coordinate, was_x);
+    }
     return 0;
 }
---
+++ src/ttt/tictactoe_board.hpp
@@ -0,0 +1,74 @@
+#ifndef LEARNING_PROJECTS_TICTACTOE_BOARD_HPP
+#define LEARNING_PROJECTS_TICTACTOE_BOARD_HPP
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <ostream>
+#include <vector>
+
+struct tictactoe_coordinate {
+    int x;
+    int y;
+
+    bool operator==(const tictactoe_coordinate &other) const {
+        return x == other.x && y == other.y;
+    }
+};
+
+inline bool vector_contains(const std::vector<tictactoe_coordinate> &vector, const tictactoe_coordinate element) {
+    const int place = std::ranges::count(vector.begin(), vector.end(), element);
+    return place > 0;
+}
+
+class tictactoe_board {
+    std::vector<tictactoe_coordinate> free_cells = std::vector<tictactoe_coordinate>();
+    std::vector<tictactoe_coordinate> x_occupied_cells = std::vector<tictactoe_coordinate>();
+    std::vector<tictactoe_coordinate> o_occupied_cells = std::vector<tictactoe_coordinate>();
+
+public:
+    constexpr static int board_size = 3;
+
+    explicit tictactoe_board() {
+        for (int x = 0; x < board_size; x++) {
+            for (int y = 0; y < board_size; y++) {
+                free_cells.push_back({x, y});
+            }
+        }
+    }
+
+    void draw_board() const {
+        std::cout << "\033[2J\033[H";
+        for (auto y = 0; y < board_size; ++y) {
+            for (auto x = 0; x < board_size; ++x) {
+                const tictactoe_coordinate coordinate = {x, y};
+                if (x > 0) {
+                    std::cout << "|";
+                }
+                std::cout << " " << (vector_contains(x_occupied_cells, coordinate) ? "X" : vector_contains(o_occupied_cells, coordinate) ? "O" : " ") << " ";
+            }
+            std::cout << "\n";
+            if (y < board_size - 1) {
+                std::cout << "---|---|---\n";
+            }
+        }
+    }
+
+    void play_move(const tictactoe_coordinate &coordinate, const bool is_x) {
+        if (vector_contains(free_cells, coordinate)) {
+            free_cells.erase(std::ranges::find(free_cells, coordinate));
+            if (is_x) {
+                x_occupied_cells.push_back(coordinate);
+            } else {
+                o_occupied_cells.push_back(coordinate);
+            }
+        } else {
+            std::cout << "Already occupied, try again." << std::endl;
+        }
+    }
+
+    [[nodiscard]] bool is_occupied(const tictactoe_coordinate &coordinate) const {
+        return !vector_contains(free_cells, coordinate);
+    }
+};
+
+#endif // LEARNING_PROJECTS_TICTACTOE_BOARD_HPP
