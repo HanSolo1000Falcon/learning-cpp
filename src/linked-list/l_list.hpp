3103456054209d4bed9f59ef8871ac19ead907e5
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2026-03-12 16:17:15 +0100

    Made new linked list.

---
+++ src/linked-list/l_list.hpp
@@ -0,0 +1,196 @@
+/*
+ * Took some inspiration from C#'s `List<T>`
+ * Still like an `std::vector<typename T>` but actually somewhat usable.
+ */
+
+#ifndef LEARNING_PROJECTS_L_LIST_HPP
+#define LEARNING_PROJECTS_L_LIST_HPP
+#include <stdexcept>
+
+template<typename T>
+class l_list {
+    struct node {
+        T *data;
+        node *next;
+        node *previous;
+
+        ~node() {
+            if (data != nullptr) {
+                // Freeing here instead of deleting because `malloc` is used when adding
+                free(data);
+            }
+        }
+    };
+
+    struct node_iterator {
+        node *current;
+
+        node_iterator(node *current) : current(current) {}
+
+        T operator*() const { return *(current->data); }
+        bool operator!=(const node_iterator &other) const { return current != other.current; }
+
+        node_iterator &operator++() {
+            current = current->next;
+            return *this;
+        }
+
+        node_iterator &operator--() {
+            current = current->previous;
+            return *this;
+        }
+    };
+
+    node *sentry;
+
+    node *get_node_internal(int index) {
+        if (index >= size() || index < 0) {
+            throw std::out_of_range("Specified index is out of range.");
+        }
+
+        auto current = sentry->next;
+        for (int i = 0; i < index; ++i) {
+            current = current->next;
+        }
+        return current;
+    }
+
+public:
+
+    l_list() {
+        sentry = new node();
+        sentry->data = nullptr;
+        sentry->next = sentry;
+        sentry->previous = sentry;
+    }
+
+    ~l_list() {
+        auto current = sentry->next;
+        while (current != sentry) {
+            auto next = current->next;
+            delete current;
+            current = next;
+        }
+    }
+
+    void add(T data) {
+        T *data_pointer = static_cast<T *>(malloc(sizeof(T)));
+        *data_pointer = data;
+
+        const auto new_node = new node();
+        new_node->data = data_pointer;
+        sentry->previous->next = new_node;
+        new_node->previous = sentry->previous;
+        new_node->next = sentry;
+        sentry->previous = new_node;
+    }
+
+    void remove_first(T data) {
+        for (auto it = begin(); it != end(); ++it) {
+            node *current = it.current;
+            if (*(current->data) == data) {
+                current->previous->next = current->next;
+                current->next->previous = current->previous;
+                delete current;
+                return;
+            }
+        }
+    }
+
+    void remove_last(T data) {
+        auto it = rbegin();
+        while (it != rend()) {
+            node *current = it.current;
+            ++it;
+            if (*(current->data) == data) {
+                current->previous->next = current->next;
+                current->next->previous = current->previous;
+                delete current;
+                return;
+            }
+        }
+    }
+
+    void remove_all(T data) {
+        auto it = rbegin();
+        while (it != rend()) {
+            node *current = it.current;
+            ++it;
+            if (*(current->data) == data) {
+                current->previous->next = current->next;
+                current->next->previous = current->previous;
+                delete current;
+            }
+        }
+    }
+
+    void remove_at(const int index) {
+        if (index >= size() || index < 0) {
+            throw std::out_of_range("Specified index is out of range.");
+        }
+        auto to_delete = get_node_internal(index);
+        auto previous = to_delete->previous;
+        auto next = to_delete->next;
+        previous->next = next;
+        next->previous = previous;
+        delete to_delete;
+    }
+
+    T find_first(T data) {
+        for (T foo_data : *this) {
+            if (foo_data == data) {
+                return foo_data;
+            }
+        }
+        throw std::out_of_range("Specified data was not found.");
+    }
+
+    T find_last(T data) {
+        for (auto it = rbegin(); it != rend(); --it) {
+            if (*it == data) {
+                return *it;
+            }
+        }
+        throw std::out_of_range("Specified data was not found.");
+    }
+
+    l_list find_all(T data) {
+        l_list found_list;
+        for (T foo_data : *this) {
+            if (foo_data == data) {
+                found_list.add(foo_data);
+            }
+        }
+        return found_list;
+    }
+
+    T operator[](int index) {
+        if (index >= size() || index < 0) {
+            throw std::out_of_range("Specified index is out of range.");
+        }
+
+        for (T data : *this) {
+            if (index == 0) {
+                return data;
+            }
+            --index;
+        }
+
+        throw std::out_of_range("Specified index is out of range.");
+    }
+
+    size_t size() {
+        size_t size = 0;
+        for (T data : *this) {
+            ++size;
+        }
+        return size;
+    }
+
+    [[nodiscard]] node_iterator begin() const { return {sentry->next}; }
+    [[nodiscard]] node_iterator end() const { return {sentry}; }
+    [[nodiscard]] node_iterator rbegin() const { return node_iterator(sentry->previous); }
+    [[nodiscard]] node_iterator rend() const { return node_iterator(sentry); }
+};
+
+#endif // LEARNING_PROJECTS_L_LIST_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,36 @@
-#include "bitmap-visualizer/bv_manager.hpp"
+#include <iostream>
+#include <ostream>
+
+#include "linked-list/l_list.hpp"
+
+template<typename T>
+void print_list(const l_list<T> &list) {
+    for (const T i: list) {
+        std::cout << i;
+    }
+    std::cout << std::endl;
+}
 
 int main() {
-    auto bv = bv_manager();
-    bv.render_bitmap();
+    auto int_list = l_list<int>();
+    for (int i = 0; i < 10; ++i) {
+        int_list.add(1);
+        int_list.add(2);
+        int_list.add(3);
+        int_list.add(4);
+        int_list.add(5);
+    }
+    print_list(int_list);
+    int_list.remove_first(2);
+    print_list(int_list);
+    int_list.remove_last(5);
+    print_list(int_list);
+    int_list.remove_all(3);
+    print_list(int_list);
+    int_list.remove_at(2);
+    print_list(int_list);
+    std::cout << int_list.find_first(2) << std::endl;
+    std::cout << int_list.find_last(2) << std::endl;
+    print_list(int_list.find_all(2));
     return 0;
 }
